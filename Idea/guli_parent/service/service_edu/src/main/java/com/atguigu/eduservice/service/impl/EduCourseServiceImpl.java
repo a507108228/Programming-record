37eb4b59f5aea43b4502bf086b9e507f03b7dba4
liziwei/Teaching.git <507109228@qq.com>
2021-10-15 17:50:44 +0800

    谷粒学院第七天

--- Idea/guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduCourseServiceImpl.java
+++ Idea/guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduCourseServiceImpl.java
@@ -1,9 +1,12 @@
 package com.atguigu.eduservice.service.impl;
 
 import com.atguigu.eduservice.entity.EduCourse;
+import com.atguigu.eduservice.entity.vo.CourseInfoVo;
 import com.atguigu.eduservice.mapper.EduCourseMapper;
 import com.atguigu.eduservice.service.EduCourseService;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +20,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class EduCourseServiceImpl extends ServiceImpl<EduCourseMapper, EduCourse> implements EduCourseService {
 
+    // 添加课程的基本信息
+    @Override
+    public void saveCourseInfo(CourseInfoVo courseInfoVo){
+        // 1像课程表添加课程的基本信息
+        EduCourse eduCourse = new EduCourse();
+        // courseInfoVo 对象转换为eduCourse对象
+        BeanUtils.copyProperties(courseInfoVo, eduCourse);
+        int insert = baseMapper.insert(eduCourse);
+        if (insert == 0) {
+            throw new GuliException(20001, "添加课程信息失败");
+
+        }
+        // 2像课程简介表添加课程简介
+
+    }
 }
