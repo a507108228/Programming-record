5eef54aad23c50cd73df15cb275f9e199a54a813
liziwei/Teaching.git <507109228@qq.com>
2021-10-25 16:57:15 +0800

    10/25提交

---
+++ Idea/guli_parent/service/service_vod/src/main/java/com/atguigu/vod/utils/InitVodClient.java
@@ -0,0 +1,20 @@
+package com.atguigu.vodtest;
+
+import com.aliyun.oss.ClientException;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.profile.DefaultProfile;
+
+/**
+ * @author a5071
+ * @since 2021/10/23 17:43
+ * description:
+ */
+public class InitObject{
+
+    public static DefaultAcsClient initVodClient(String accessKeyId, String accessKeySecret) throws ClientException{
+        String regionId = "cn-shanghai";  // 点播服务接入区域
+        DefaultProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, accessKeySecret);
+        DefaultAcsClient client = new DefaultAcsClient(profile);
+        return client;
+    }
+}
---
+++ Idea/guli_parent/service/service_vod/src/test/java/com/atguigu/vodtest/InitObject.java
@@ -0,0 +1,7 @@
+package com.atguigu.vodtest;/**
+ * @author a5071
+ * @since 2021/10/23 17:43
+ * description: 
+ *
+ */public class InitObject {
+}
